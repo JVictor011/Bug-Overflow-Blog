dd8d4ed0e2ecf2348d1133230e83cb39ff7d7373
JVictor011 <jvictor2021@alu.ufc.br>
2024-05-07 21:51:43 -0300

    articles

--- artides/ArtidesController.js
+++ artides/ArtidesController.js
@@ -4,36 +4,90 @@ const Articles = require("./Articles");
 const slugify = require("slugify");
 const Category = require("../categories/Category");
 
-router.get("/articles", (req, res) => {
-  res.send("articles");
+router.get("/admin/articles", (req, res) => {
+  try {
+    Articles.findAll({
+      include: [{ model: Category }],
+    }).then((articles) => {
+      res.render("admin/articles/index", { articles: articles });
+    });
+  } catch (erro) {
+    res.status(500).send("Erro ao recuperar artigos");
+  }
 });
 
-router.get("/admin/articles/new", async (req, res) => {
+router.get("/admin/articles/new", (req, res) => {
   try {
-    const categoris = await Category.findAll();
-    res.render("admin/articles/new", { categories: categoris });
+    Category.findAll().then((categoris) => {
+      res.render("admin/articles/new", { categories: categoris });
+    });
   } catch (erro) {
     res.status(500).send("Erro ao recuperar categorias");
   }
 });
 
 router.post("/articles/save", (req, res) => {
-  var [title, body] = req.body;
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
   if (title != undefined || body != undefined) {
     Articles.create({
       title: title,
       slug: slugify(title).toLowerCase(),
       body: body,
+      categoriaId: category,
     }).then(() => {
-      res.redirect("/artigos");
+      res.redirect("/admin/articles");
     });
   } else {
-    res.redirect("/artigos/new");
+    res.redirect("/admin/artigos/new");
   }
 });
 
-router.get("/artigos", (req, res) => {
-  res.send("artides");
+router.post("/articles/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Articles.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      res.redirect("/admin/articles");
+    }
+  } else {
+    res.redirect("/admin/articles");
+  }
+});
+
+router.get("/admin/articles/edit/:id", async (req, res) => {
+  try {
+    var id = req.params.id;
+
+    if (isNaN(id)) {
+      res.redirect("/admin/articles");
+    }
+
+    const article = await Articles.findByPk(id, {
+      include: [{ model: Category }],
+    });
+
+    if (!article) {
+      return res.redirect("/admin/articles");
+    }
+
+    const categories = await Category.findAll();
+
+    res.render("admin/articles/edit", {
+      article: article,
+      categories: categories,
+    });
+  } catch (erro) {
+    res.status(500).send("Erro ao carregar página de edição.");
+  }
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("bug-overflow-blog", "root", "jovi", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 module.exports = connection;
