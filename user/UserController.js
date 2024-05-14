df5b200963ccee398b6d5fe8b03a82037dbd1109
JVictor011 <jvictor2021@alu.ufc.br>
2024-05-13 22:40:40 -0300

    hash

--- user/UserController.js
+++ user/UserController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
   //   User.findAll().then((users) => {
@@ -10,6 +11,24 @@ router.get("/admin/users", (req, res) => {
 
 router.get("/admin/users/create", (req, res) => {
   res.render("admin/users/create");
+});
+
+router.post("/users/create", (req, res) => {
+  const { username, password } = req.body;
+
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+
+  User.create({
+    username: username,
+    password: hash,
+  })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      res.status(500).send("Erro ao cadastrar usuário.");
+    });
 });
 
 module.exports = router;
